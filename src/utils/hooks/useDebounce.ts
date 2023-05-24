f685023bea8075948d3d661f21992c0843c31e85
Mauricio Girardi <maurigirarde@yahoo.com.br>
2023-05-24 15:46:31 +0100

    create debounce

---
+++ src/utils/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handle);
+    };
+  }, [delay, value]);
+
+  return debouncedValue;
+}
